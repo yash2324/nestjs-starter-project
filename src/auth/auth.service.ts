9b476d0db7fff5cf46afc377803323899d600da9
yash2324 <yg232004@gmail.com>
2025-06-24 15:33:39 +0530

    fixes: fixed lodash types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import _ from 'lodash';
+import { defaultTo, isEqual } from 'lodash';
 import { DEFAULT_PASSWORD, DEFAULT_USERNAME } from 'src/constants';
+
 @Injectable()
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
   async validateUser(username: string, password: string): Promise<boolean> {
-    const validUsername = _.defaultTo(
+    const validUsername = defaultTo(
       process.env.BASIC_AUTH_USER,
       DEFAULT_USERNAME,
     );
-    const validPassword = _.defaultTo(
+    const validPassword = defaultTo(
       process.env.BASIC_AUTH_PASS,
       DEFAULT_PASSWORD,
     );
-    return (
-      _.isEqual(username, validUsername) && _.isEqual(password, validPassword)
-    );
+    return isEqual(username, validUsername) && isEqual(password, validPassword);
   }
 
   async login(user: { username: string }) {
